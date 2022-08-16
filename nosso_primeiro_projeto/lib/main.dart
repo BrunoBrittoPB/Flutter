d4ddbf33cb81945bd8689fdfe8a152b7c2f1a5eb
Lucas Veríssimo Leite <lucass.verissimo@gmail.com>
2022-08-16 14:24:09 -0300

    teste de commit.

--- nosso_primeiro_projeto/lib/main.dart
+++ nosso_primeiro_projeto/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> { 
 
   bool opacidade = true;
 
